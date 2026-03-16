370ddc9017bdeb7aa914107aefee4be70affee1c
Pete-Tuakana <tuakana80@gmail.com>
2026-03-17 07:43:34 +1000

    Update app.js

--- app.js
+++ app.js
@@ -22,3 +22,39 @@ let words = text.split(" ");
 return words[0];
 
 }
+
+function getVendorMemory(){
+
+let memory = localStorage.getItem("vendorMemory");
+
+if(memory){
+return JSON.parse(memory);
+}
+
+return {};
+
+}
+
+function saveVendorMemory(memory){
+
+localStorage.setItem("vendorMemory", JSON.stringify(memory));
+
+}
+
+function learnVendor(vendor){
+
+let memory = getVendorMemory();
+
+if(memory[vendor]){
+return memory[vendor];
+}
+
+let category = prompt("Assign category for: " + vendor);
+
+memory[vendor] = category;
+
+saveVendorMemory(memory);
+
+return category;
+
+}
